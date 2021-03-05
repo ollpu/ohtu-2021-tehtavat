dc0bb0708fc7772cba7324ef69ba7b0def7cf282
Roope Salmi <rpsalmi@gmail.com>
2021-03-05 19:10:35 +0200

    Tehtävä 1

---
+++ viikko3/nhl-reader/src/index.py
@@ -0,0 +1,29 @@
+import requests
+from datetime import datetime
+from player import Player
+
+def main():
+    url = "https://nhlstatisticsforohtu.herokuapp.com/players"
+    response = requests.get(url).json()
+
+    players = []
+    wanted_nationality = "FIN"
+
+    for player_dict in response:
+        if player_dict['nationality'] == wanted_nationality:
+            player = Player(
+                player_dict['name'],
+                player_dict['team'],
+                player_dict['goals'],
+                player_dict['assists'],
+            )
+
+            players.append(player)
+
+    print(f"Players from {wanted_nationality} {datetime.now()}\n")
+
+    for player in players:
+        print(player)
+
+if __name__ == "__main__":
+    main()
